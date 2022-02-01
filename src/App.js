268e4e9a64c461abcc0d1742c5c911c7b33d5ea2
Ankur Sharma <ankursharma.as0403@gmail.com>
2022-02-02 02:47:50 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-// import About from "./components/About";
+import About from "./components/About";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
     const [mode, setMode] = useState("light"); //whether dark mode is enable or not
@@ -22,29 +23,42 @@ function App() {
             setMode("dark");
             document.body.style.backgroundColor = "#042743";
             showAlert("Dark mode has been enabled", "success");
+            document.title = "TextUtils-Dark Mode";
         } else {
             setMode("light");
             document.body.style.backgroundColor = "white";
             showAlert("Light mode has been enabled", "success");
+            document.title = "TextUtils-Light Mode";
         }
     };
     return (
         <>
-            <Navbar
-                title="TextUtils"
-                aboutText="About TextUtils"
-                mode={mode}
-                toggleMode={toggleMode}
-            />
-            <Alert alert={alert} />
-            <div className="container my-3">
-                <TextForm
-                    showAlert={showAlert}
-                    heading="Enter the text to analyze below"
+            <Router>
+                <Navbar
+                    title="TextUtils"
+                    aboutText="About TextUtils"
                     mode={mode}
+                    toggleMode={toggleMode}
                 />
-                {/*<About />*/}
-            </div>
+                <Alert alert={alert} />
+                <div className="container my-3">
+                    <Routes>
+                        <Route exact path="/about" element={<About />} />
+
+                        <Route
+                            exact
+                            path="/"
+                            element={
+                                <TextForm
+                                    showAlert={showAlert}
+                                    heading="Enter the text to analyze below"
+                                    mode={mode}
+                                />
+                            }
+                        />
+                    </Routes>
+                </div>
+            </Router>
         </>
     );
 }
